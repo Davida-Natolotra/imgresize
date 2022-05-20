b980777eb2aefce5e17f803b77cf8aef6ff90104
Davida-Natolotra <66999620+Davida-Natolotra@users.noreply.github.com>
2022-05-20 04:45:23 +0300

    Add previous size and new size, with max width = 720

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Resizer from "react-image-file-resizer";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			newImage: "",
+			newSize: 0,
+		};
+	}
+
+	previousSize = 0;
+
+	fileChangedHandler = (event) => {
+		var fileInput = false;
+		if (event.target.files[0]) {
+			fileInput = true;
+			this.previousSize = event.target.files[0].size;
+		}
+		if (fileInput) {
+			try {
+				const img = Resizer.imageFileResizer(
+					event.target.files[0],
+					720,
+					500,
+					"JPEG",
+					100,
+					0,
+					(uri) => {
+						console.log(uri);
+						this.setState({ newImage: uri });
+						const base64str = uri.split("base64,")[1];
+						const decoded = atob(base64str);
+						this.setState({ newSize: decoded.length });
+					},
+					"base64",
+					200,
+					200
+				);
+				console.log("img: ", typeof img);
+			} catch (err) {
+				console.log(err);
+			}
+		}
+	};
+
+	componentDidUpdate(prevState) {
+		if (prevState.newImage !== this.state.newImage) {
+			console.log("FileSize: " + this.state.newSize);
+		}
+	}
+
+	render() {
+		return (
+			<div className="App">
+				<input type="file" onChange={this.fileChangedHandler} />
+				<img src={this.state.newImage} alt="" />
+				<h2>Previous size: {this.previousSize}</h2>
+				<h2>New size: {this.state.newSize}</h2>
+			</div>
+		);
+	}
 }
 
 export default App;
